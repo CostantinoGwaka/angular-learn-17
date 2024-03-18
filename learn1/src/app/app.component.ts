0b6f0d933e250505bab020dacfed026661890204
Costantino Dionis Gwaka <gwaka94@gmail.com>
2024-03-18 12:02:44 +0300

    fix

--- learn1/src/app/app.component.ts
+++ learn1/src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { AfterContentInit, Component } from '@angular/core';
+import { AfterContentInit, Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterContentInit {
+
+// AfterContentInit
+export class AppComponent implements DoCheck {
   title = 'learn1';
 
   dataFromParent = '';
 
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit was invoked');
+  // ngAfterContentInit(): void {
+  //   console.log('ngAfterContentInit was invoked');
+  // }
+
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck was invoked');
   }
 
 
--- learn1/src/app/test/test.component.ts
+++ learn1/src/app/test/test.component.ts
@@ -1,13 +1,25 @@
-import { Component, DoCheck } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, DoCheck, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss']
 })
-export class TestComponent implements DoCheck {
 
-  ngDoCheck(): void {
-    console.log('ngDoCheck is invoked');
+// DoCheck
+export class TestComponent implements AfterContentInit {
+
+  @ViewChild('wrapper') wrapper!: ElementRef;
+  @ContentChild('contentWrapper') content!: ElementRef
+
+  // ngDoCheck(): void {
+  //   console.log('ngDoCheck is invoked');
+  // }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit is invoked');
+    console.log('ngAfterContentInit is wrapper', this.wrapper);
+    console.log('ngAfterContentInit is content', this.content);
+
   }
 }
