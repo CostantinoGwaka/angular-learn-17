10255d50265deae397ab6022f3ecdbbf00b1fd11
Costantino Dionis Gwaka <gwaka94@gmail.com>
2024-03-19 21:56:21 +0300

    fix

--- learn1/src/app/test/test.component.ts
+++ learn1/src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, ContentChild, DoCheck, ElementRef, ViewChild } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -7,19 +7,38 @@ import { AfterContentInit, Component, ContentChild, DoCheck, ElementRef, ViewChi
 })
 
 // DoCheck
-export class TestComponent implements AfterContentInit {
+export class TestComponent implements AfterContentInit, AfterContentChecked, AfterViewInit, OnInit {
 
   @ViewChild('wrapper') wrapper!: ElementRef;
   @ContentChild('contentWrapper') content!: ElementRef
 
-  // ngDoCheck(): void {
-  //   console.log('ngDoCheck is invoked');
-  // }
+  ngDoCheck(): void {
+    console.log('ngDoCheck is invoked');
+  }
+
+  ngOnInit(): void {
+    //hook will initialize the component when it gets fully rendered & that does 
+    //not imply on view getting fully rendered so this might not give the appropriate result
+  }
 
   ngAfterContentInit(): void {
     console.log('ngAfterContentInit is invoked');
     console.log('ngAfterContentInit is wrapper', this.wrapper);
     console.log('ngAfterContentInit is content', this.content);
 
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked was invoked');
+  }
+
+  ngAfterViewInit(): void {
+    //used perform task on after view initialization
+    const divElement: HTMLElement = this.wrapper.nativeElement;
+    divElement.style.color = 'blue';
+    divElement.style.fontSize = '15px';
+    divElement.style.fontWeight = '300';
+
+    console.log('ngAfterViewInit was invoked', this.wrapper);
   }
 }
